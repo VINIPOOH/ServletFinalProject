37f913d8f4dff596e8e72b2ff0851c73fc1ef797
VINIPOOH <47758583+VINIPOOH@users.noreply.github.com>
2020-05-21 12:58:20 +0300

    Corrected internationalization

--- delivery/src/main/java/web/comand/action/impl/UserStatistic.java
+++ delivery/src/main/java/web/comand/action/impl/UserStatistic.java
@@ -2,10 +2,10 @@ package web.comand.action.impl;
 
 import bl.service.BillService;
 import dal.entity.User;
+import dto.validation.IDValidator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import web.comand.action.ActionCommand;
-import web.dto.validation.IDValidator;
 import web.util.Pagination;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,13 +16,15 @@ import static web.constants.PageConstance.*;
 
 public class UserStatistic implements ActionCommand {
 
-    private static final String BILLS_LIST = "billsList";
     public static final String USER_USER_STATISTIC = "user/user-statistic";
-    private final Pagination pagination;
+    private static final String BILLS_LIST = "billsList";
+    public static final int PAGE_ATRIBUTE = 1;
+    public static final int PAGE_SIZE = 10;
     private static Logger log = LogManager.getLogger(UserStatistic.class);
-
+    private final Pagination pagination;
     private final IDValidator idValidator;
     private final BillService billService;
+
 
     public UserStatistic(Pagination pagination, IDValidator idValidator, BillService billService) {
         this.pagination = pagination;
@@ -33,14 +35,15 @@ public class UserStatistic implements ActionCommand {
     @Override
     public String execute(HttpServletRequest request) {
         log.debug("");
-
-        if (!pagination.Validate(request)) {
-            return REDIRECT_COMMAND + ANONYMOUS_FOLDER + ERROR_404_FILE_NAME;
+        int pageAtribute = PAGE_ATRIBUTE;
+        int pageSize = PAGE_SIZE;
+        if (pagination.Validate(request)) {
+            pageAtribute = Integer.parseInt(request.getParameter("page"));
+            pageSize = Integer.parseInt(request.getParameter("size"));
         }
-        int pageAtribute = Integer.parseInt(request.getParameter("page"));
-        int pageSize = Integer.parseInt(request.getParameter("size"));
 
         pagination.paginate(pageAtribute, pageSize, billService.countAllBillsBiUserId(((User) request.getSession().getAttribute(SESSION_USER)).getId()), request, USER_USER_STATISTIC);
+        //todo rework offset
         request.setAttribute(BILLS_LIST, billService.getBillHistoryByUserId(((User) request.getSession().getAttribute(SESSION_USER)).getId(), (pageAtribute - 1) * pageSize, pageSize));
         return MAIN_WEB_FOLDER + USER_FOLDER + USER_STATISTIC_FILE_NAME;
     }
