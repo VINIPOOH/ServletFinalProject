93dc7f9742c86b69e12f9aafcecf2d99abca447c
VINIPOOH <47758583+VINIPOOH@users.noreply.github.com>
2020-07-02 17:03:52 +0300

    counter corrected

--- delivery/src/main/webapp/js/calculator.js
+++ delivery/src/main/webapp/js/calculator.js
@@ -1,11 +1,10 @@
 window.addEventListener("load", bindActionsToDocument);
-alert("test");
 function bindActionsToDocument (){
-    document.getElementById("localitySandID").addEventListener("change", function () {
+    document.getElementById("localitySandIDSelect").addEventListener("change", function () {
         var selectedIndex =this.selectedIndex;
         if (selectedIndex < 0){
             return;
         }
         alert(this.options[selectedIndex].value);
     });
-};
+}
